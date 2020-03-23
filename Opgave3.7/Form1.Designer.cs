11e170b515b534b572f290c31ddb9224b77d0f05
skrrrm <a@macbook-windows>
2020-03-23 11:16:30 +0100

    Add project files.

---
+++ Opgave3.7/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Opgave3._7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskedTextBoxInput = new System.Windows.Forms.MaskedTextBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // maskedTextBoxInput
+            // 
+            this.maskedTextBoxInput.Location = new System.Drawing.Point(408, 78);
+            this.maskedTextBoxInput.Mask = "00-00-0000";
+            this.maskedTextBoxInput.Name = "maskedTextBoxInput";
+            this.maskedTextBoxInput.Size = new System.Drawing.Size(299, 31);
+            this.maskedTextBoxInput.TabIndex = 0;
+            this.maskedTextBoxInput.ValidatingType = typeof(System.DateTime);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(12, 81);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(275, 25);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Type the date in the format:";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(408, 181);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(156, 53);
+            this.buttonOk.TabIndex = 2;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(17, 298);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(0, 25);
+            this.labelDate.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.maskedTextBoxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxInput;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Label labelDate;
+    }
+}
+
---
+++ Opgave3.7/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opgave3._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            // Variables
+            string _date = maskedTextBoxInput.Text;
+            string _day = _date.Substring(0, 2);
+            string _month = _date.Substring(3, 2);
+            string _year = _date.Substring(6, 4);
+            //string _monthText = "";
+            //string changed_date = $"{_day}-{_month}-{_year}";
+
+            switch (_month)
+            {
+                case "01":
+                    _month = "January";
+                    break;
+                case "02":
+                    _month = "February";
+                    break;
+                case "03":
+                    _month = "March";
+                    break;
+                case "04":
+                    _month = "April";
+                    break;
+                case "05":
+                    _month = "May";
+                    break;
+                case "06":
+                    _month = "June";
+                    break;
+                case "07":
+                    _month = "July";
+                    break;
+                case "08":
+                    _month = "August";
+                    break;
+                case "09":
+                    _month = "September";
+                    break;
+                case "10":
+                    _month = "Oktober";
+                    break;
+                case "11":
+                    _month = "November";
+                    break;
+                case "12":
+                    _month = "December";
+                    break;
+                default:
+                    labelDate.Text = "Something went wrong!";
+                    break;
+            }
+            labelDate.Text = $"The date is {_day}st of {_month} {_year}";
+        }
+    }
+}
